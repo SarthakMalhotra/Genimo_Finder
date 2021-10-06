7dccd446ae6a337ad279ff5e62727535ac9e390b
SarthakMalhotra <66949270+SarthakMalhotra@users.noreply.github.com>
2021-10-06 14:26:20 +0530

    For Hackers Who Want More Links Than One

---
+++ beta.py
@@ -0,0 +1,45 @@
+#Credits To HackerSarthak
+import requests
+import urllib
+from urllib.request import *
+from urllib.error import *
+from colorama import Fore
+import os
+from urllib.error import HTTPError
+from urllib.request import urlopen, HTTPError
+import time
+
+
+links = open('links.txt', 'r')
+
+banner = Fore.GREEN + """
+ ██████  ███████ ███    ██ ██ ███    ███  ██████  
+██       ██      ████   ██ ██ ████  ████ ██    ██ 
+██   ███ █████   ██ ██  ██ ██ ██ ████ ██ ██    ██ 
+██    ██ ██      ██  ██ ██ ██ ██  ██  ██ ██    ██ 
+ ██████  ███████ ██   ████ ██ ██      ██  ██████  
+ """
+os.system('cls')
+print(banner)
+print(Fore.YELLOW+" The Best Admin Panel Finder With Over 421 Links!")
+print("")
+link = (input(Fore.RED+"Enter Website Address : "))
+
+while True:
+	sublink = links.readline().split('\n') [0]
+
+	if not sublink:
+		break
+
+	reqlink = 'http://'+link+'/'+sublink
+
+	try:
+		response = urlopen(reqlink)
+	except HTTPError as e:
+		print(Fore.RED+"[-] "+reqlink+"    ")
+		continue
+	except URLError as e:
+		print(Fore.RED+"[-] "+reqlink+"    ")
+		continue
+	else:
+		print(Fore.GREEN+"[+] "+reqlink)
